3ed3e89e1a8ba6e5bbc5c6ded0584f020ed3e2ed
ChangJoo Park <pcjpcj2@gmail.com>
2018-07-04 23:33:41 +0900

    Update catch-exception.js

--- src/catch-exception.js
+++ src/catch-exception.js
@@ -1,8 +1,5 @@
-module.exports = (e, remarks) => {
-  if (this.requestUri === '') {
-    return this
-  }
-  var message = e.message || ''
-  var e = e.stack || ''
-  return this.send(message, '', -1, -1, e)
+const send = require('./send')
+
+module.exports = function (error) {
+  send(error)
 }
