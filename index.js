2aa7f67d5555dd5d5ea244e0f852c2cd38712a52
ChangJoo Park <pcjpcj2@gmail.com>
2018-07-04 23:30:34 +0900

    Update index.js for module and browser

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-window.BladeShield = {
+const BladeShield = {
   requestUri: '',
   remarks: [],
   errors: {
     ISSUE_ERROR: 'ISSUE_ERROR',
     SCRIPT_ERROR: 'Script error'
   },
-  send: require('./src/send'),
-  validateUri: require('./src/validate-uri'),
-  validateIssue: require('./src/validate-issue'),
-  setUri: require('./src/set-uri'),
+  config: require('./src/config'),
+  install: require('./src/install'),
   setRemarks: require('./src/set-remarks'),
   catchException: require('./src/catch-exception')
 }
+window.BladeShield = BladeShield
+module.exports = BladeShield
