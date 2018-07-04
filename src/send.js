4e6a8b8753e41166461b068ebb514bc828385e35
ChangJoo Park <pcjpcj2@gmail.com>
2018-07-04 23:31:46 +0900

    WIP update send.js

--- src/send.js
+++ src/send.js
@@ -1,11 +1,5 @@
-module.exports = (message, source, lineno, colno, e) => {
+module.exports = function (message, source, lineno, colno, e) {
   if (this.requestUri === '') {
-    return this
-  }
-
-  const issue = this.validateIssue(message, source, lineno, colno, e)
-
-  if (issue.message === 'Script error') {
     return this
   }
 
