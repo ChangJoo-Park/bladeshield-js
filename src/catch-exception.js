880d7c45ee82c8932011d8e8c4d682d6076dad24
ChangJoo Park <pcjpcj2@gmail.com>
2018-07-05 10:35:07 +0900

    parse error object. it needs for same format on window.onerror

--- src/catch-exception.js
+++ src/catch-exception.js
@@ -1,5 +1,10 @@
 const send = require('./send')
 
 module.exports = function (error) {
-  send(this.requestUri, error)
+  send(this.requestUri, {
+    name: error.name,
+    message: error.message,
+    url: document.location.href,
+    stack: error.stack
+  })
 }
