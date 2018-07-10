79850afd0b197c69b60a8d63e11673c44e697ce9
ChangJoo Park <pcjpcj2@gmail.com>
2018-07-10 17:36:05 +0900

    Update message sender

--- src/send.js
+++ src/send.js
@@ -1,11 +1,34 @@
+const pckg = require('../package.json')
+
 module.exports = (uri, error) => {
+  const {
+    message,
+    source,
+    lineno,
+    colno,
+    stack
+  } = error
+
+  const payload = {
+    message,
+    source,
+    lineno,
+    colno,
+    stack,
+    version: pckg.version
+  }
+
   fetch(uri, {
-    method: 'POST',
-    body: JSON.stringify(error)
-  }).then(response => {
-    return response.json()
-  }).then(data => {
-    console.log('Created Gist:', data.html_url)
-  })
-  .catch(e => console.error(e))
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      method: 'POST',
+      body: JSON.stringify(payload)
+    }).then(response => {
+      return response.json()
+    }).then(data => {
+      console.log('data => ', data)
+    })
+    .catch(e => console.error(e))
 }
